8fb6d25ba27f1333d5379d859369b3a0e219d6a1
thanmaisai <dev.thanmaisai@gmail.com>
2025-04-16 20:11:49 +0530

    Changing Icon Libs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LucideAngularModule, Sparkles } from 'lucide-angular';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, LucideAngularModule],
+  imports: [CommonModule, FontAwesomeModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Experience Studio';
-  SparklesIcon = Sparkles;
+  faStar = faStar;
 }
